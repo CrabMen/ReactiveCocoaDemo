80c59f76a685d8b59c2d4547f0276c9bcbb148cb
Crab-Man <tobecrabman@163.com>
2016-06-06 19:35:24 +0800

    webView从H5界面返回
    1.通过判断webView的canGoback属性来判断是否可以返回
    2.通过三方库设置下拉刷新

---
+++ MVVM+RAC/ReactiveCocoaBase/Pods/Target Support Files/Pods-ReactiveCocoaBase/Pods-ReactiveCocoaBase-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ReactiveCocoaBaseVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ReactiveCocoaBaseVersionString[];
+
---
+++ MVVM+RAC/ReactiveCocoaBase/ReactiveCocoaBase/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ReactiveCocoaBase
+//
+//  Created by CrabMan on 16/6/6.
+//  Copyright © 2016年 CrabMan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
